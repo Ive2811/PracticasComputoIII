34beaae041b0046405972b6a79b49efba0b2df3c
ive2811 <anau2649@gmail.com>
2022-04-29 23:06:51 -0600

    Se agrega el ejercicio 2.

--- SEMANA15-IntroduccionNodeJS/EjerciciosGuia/app.js
+++ SEMANA15-IntroduccionNodeJS/EjerciciosGuia/app.js
@@ -1,1 +1,13 @@
 const {comidas} = require('./ejercicio3.js');
+
+const {operacion1}=require('./ejercicio2.js')
+
+const operacion = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+operacion.question("¿Que operacion necesita realizar? \n 1. Suma \n 2. Resta \n 3. Multiplicación \n 4. División \n", operacion => {
+    operacion1(operacion)
+   
+});
--- SEMANA15-IntroduccionNodeJS/EjerciciosGuia/ejercicio2.js
+++ SEMANA15-IntroduccionNodeJS/EjerciciosGuia/ejercicio2.js
@@ -5,3 +5,28 @@ console.log("-----------Ejercicio 2.------------");
 console.log(" ");
 
 
+exports.operacion1=(operacion)=>{
+
+    const numeros = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    numeros.question("¿Cuál es su primer valor? ", num1  => {
+        numeros.question("¿Cuál es su segundo valor? ", num2 => {
+            if(operacion=1){
+                console.log(num1+num2)
+                
+            }else if(operacion=2){
+                console.log(num1-num2)
+
+            }else if(operacion=3){
+                console.log(num1*num2)
+            }else if(operacion=4){
+                console.log(num1/num2)
+            }
+        });
+    });
+    
+}
+
