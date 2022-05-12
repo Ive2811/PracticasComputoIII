0157a62696b8d40f1ea66e5a680d9df706675147
ive2811 <ivethgar1234@gmail.com>
2022-05-12 14:31:42 -0600

    Se agregan los archivos de la práctica de la semana 17.

---
+++ SEMANA17- ModulosNodeJS/converttocsv.js
@@ -0,0 +1,14 @@
+var fs = require("fs");
+var stringify = require("csv-stringify");
+
+//JSON de datos.
+var data =[
+    {"Country": "El Salvador", "Official Languages(s)": "Spanish"},
+    {"Country": "India ", "Official Languages(s)": "Hindi, English"}
+];
+
+stringify.stringify(data, {
+    header: true
+}, function (err, output) {
+    fs.writeFileSync(__dirname+"/data.csv", output);
+});
